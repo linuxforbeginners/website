3c915c3d6ada3ae9fb924e8356f41c9821f7eb23
sinmineryt <50749166+sinmineryt@users.noreply.github.com>
2021-05-31 15:22:55 +0200

    Tests: Fix article model tests

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -10,12 +10,12 @@ class ArticleTest < ActiveSupport::TestCase
   end
 
   test "should not save article with content under 10 characters" do
-    article = Article.new body: "123456789", title: "under 10 characters"
+    article = Article.new({ body: "123456789", title: "under 10 characters" })
     assert !article.save, "Article saved with under 10 characters"
   end
 
   test "should not save article without title but with content" do
-    article = Article.new body: "The content of the post"
+    article = Article.new({ body: "The content of the post" })
     assert !article.save, "Article saved without title but with content"
   end
 end
