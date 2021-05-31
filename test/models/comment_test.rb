a9ceff88926b3ef63ddd18c8f9f7d0893309ee88
sinmineryt <50749166+sinmineryt@users.noreply.github.com>
2021-05-31 15:17:59 +0200

    Tests: Add tests for the comment model

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -4,4 +4,17 @@ class CommentTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "should not save comment without username and content" do
+    article = Article.new title: "This is a test", body: "This is a comment testing article"
+    if article.save
+      assert !article.comments.create, "Comment saved without username and content"
+    end
+  end
+
+  test "should not save comment without username but content" do
+    article = Article.new title: "This is a test 2", body: "This is a comment testing article"
+    if article.save
+      assert !article.comments.create(body: "Hello, world!"), "Comment saved without username"
+    end
+  end
 end
