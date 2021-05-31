116ad38fe0029e9fe1db79dce514248305e29924
sinmineryt <50749166+sinmineryt@users.noreply.github.com>
2021-05-31 22:07:55 +0200

    Comments: Add comment validation

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,9 +2,15 @@ class CommentsController < ApplicationController
   http_basic_authenticate_with name: "admin", password: "password", except: [:create]
 
   def create
+    puts comment_params
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
-    redirect_to article_path(@article)
+    if @comment.valid?
+      redirect_to article_path(@article)
+    else
+      redirect_to article_path(@article)
+    end
+    render :create
   end
 
   def destroy
