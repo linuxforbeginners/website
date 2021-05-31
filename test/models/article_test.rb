da7c33a3f6db8ee38281846d77243ea4c39679c9
sinmineryt <50749166+sinmineryt@users.noreply.github.com>
2021-05-31 15:06:09 +0200

    Tests: Add tests for the article model

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -4,4 +4,18 @@ class ArticleTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "should not save article without title and content" do
+    article = Article.new
+    assert !article.save, "Article saved without title and content"
+  end
+
+  test "should not save article with content under 10 characters" do
+    article = Article.new body: "123456789", title: "under 10 characters"
+    assert !article.save, "Article saved with under 10 characters"
+  end
+
+  test "should not save article without title but with content" do
+    article = Article.new body: "The content of the post"
+    assert !article.save, "Article saved without title but with content"
+  end
 end
