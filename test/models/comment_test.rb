9a93d5fbddf4d8f04ee1cacf0dba64df58f13d30
sinmineryt <50749166+sinmineryt@users.noreply.github.com>
2021-05-31 15:48:44 +0200

    Tests: Fix comment model tests

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -5,16 +5,22 @@ class CommentTest < ActiveSupport::TestCase
   #   assert true
   # end
   test "should not save comment without username and content" do
-    article = Article.new title: "This is a test", body: "This is a comment testing article"
+    article = Article.new({ "title" => "This is a test", "body" => "This is a comment testing article", "status" => "public" })
+    puts "==="
+    puts article.to_json
     if article.save
       assert !article.comments.create, "Comment saved without username and content"
+    else
+      assert false, "Article saving error"
     end
   end
 
   test "should not save comment without username but content" do
-    article = Article.new title: "This is a test 2", body: "This is a comment testing article"
+    article = Article.new({ "title" => "This is a test 2", "body" => "This is a comment testing article", "status" => "public" })
     if article.save
-      assert !article.comments.create(body: "Hello, world!"), "Comment saved without username"
+      assert !article.comments.create({ "body" => "Hello, world!" }), "Comment saved without username"
+    else
+      assert false, "Article saving error"
     end
   end
 end
